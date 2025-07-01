b9dcf92df09a9dae7261b91ce3d1e7f16e91d034
frenlylm <fmezans2205@gmail.com>
2025-07-01 01:10:40 -0400

    Add files via upload

--- new fit template.py
+++ new fit template.py
@@ -3,14 +3,14 @@ plt.rcParams["font.family"] = "DejaVu Serif"
 serif = "DejaVu Serif"
 
 #all code needed to change is here!
-filename = r"\\wsl.localhost\Ubuntu\home\fmeza\heasoft-6.35.1\Xspec\data\zpup_20156\20156_grp10_helike_martins.dat"
+filename = r"filepath"
 ObsID = "ObsID  = "
 C, N, O = ["$C = $", "$N = $", "$O = $"]
-norms = ["$norm_1 = $", "$norm_2 = $", "$norm_3 = $", "$norm_4 = $", "$norm_5 = $", "$norm_6 = $"]
+norm1, norm2, norm3, norm4, norm5, norm6 = []
 sigma, cstat, bins, flux = ["$sigma = $", "$cstat = $", "$N = $", "$flux = $"]
-save_path_meg = r"yoursavepath.png"
-save_path_heg = r"yoursavepath.png"
-save_path_both = r"yoursavepath.png"
+save_path_meg = r"C:\Users\fmeza\Downloads\research data\yoursavepathmeg.png"
+save_path_heg = r"C:\Users\fmeza\Downloads\research data\yoursavepathheg.png"
+save_path_both = r"C:\Users\fmeza\Downloads\research data\yoursavepathboth.png"
 
 def readfile(filename):
     with open(filename, "r") as datfile:
@@ -69,24 +69,24 @@ def meg_action():
     plt.ylim(0, .2)
     plt.plot(histwvs[0], histmods[0], color='green', linewidth=3, label = "model")
     plt.errorbar(megwvs, megvals, xerr=megbins, yerr=megerrors, fmt='.', alpha=0.4, color='blue', linewidth=1.5, label = "data")
-    plt.annotate('ζ pup MEG group10_HEII 1', xy=(13, .15), xytext=(5.1, .19), fontsize = 18)
-    plt.annotate(ObsID, xy=(13, .15), xytext=(5.1, .17), fontsize = 18, fontname = 'serif')
+    plt.annotate('ζ pup MEG group10_HEII 1', xy=(13, .15), xytext=(5.1, .19), fontsize = 19)
+    plt.annotate(ObsID, xy=(13, .15), xytext=(5.1, .17), fontsize = 17, fontname = 'serif')
     plt.annotate(C, xy=(13, .06), xytext=(9, .19), fontsize = 17, fontname = 'serif')
     plt.annotate(N, xy=(13, .06), xytext=(10.5, .19), fontsize = 17, fontname = 'serif')
     plt.annotate(O, xy=(13, .06), xytext=(12, .19), fontsize = 17, fontname = 'serif')
-    plt.annotate(norms[0], xy=(13, .15), xytext=(14, 0.19), fontsize = 18, fontname = 'serif')
-    plt.annotate(norms[1], xy=(13, 0.18), xytext=(14, 0.175), fontsize = 18, fontname = 'serif')
-    plt.annotate(norms[2], xy=(13, .145), xytext=(14, 0.16), fontsize = 18, fontname = 'serif')
-    plt.annotate(norms[3], xy=(13, .135), xytext=(14, .145), fontsize = 18, fontname = 'serif')
-    plt.annotate(norms[4], xy=(13, .13), xytext=(14, .13), fontsize = 18, fontname = 'serif')
-    plt.annotate(norms[5], xy=(13, .145), xytext=(14, .115), fontsize = 18, fontname = 'serif')
-    plt.annotate(sigma, xy=(13, .145), xytext=(14, .1), fontsize = 18, fontname = 'serif')
-    plt.annotate(cstat, xy=(13, .14), xytext=(14, .085), fontsize = 18, fontname = 'serif')
-    plt.annotate(bins, xy=(13, .13), xytext=(16.3, .085), fontsize = 18, fontname = 'serif')
-    plt.annotate(flux, xy=(13, .055), xytext=(14, .07), fontsize = 18, fontname = 'serif')
-    plt.xlabel('Wavelength (Å)', fontsize = 18, fontname = 'serif')
-    plt.ylabel('Counts s$^{-1}$ Å$^{-1}$', fontsize = 18, fontname = 'serif')
-    #plt.savefig(save_path_meg)
+    plt.annotate(f"$norm_1 = {norm1}$", xy=(13, .15), xytext=(14, 0.19), fontsize = 19, fontname = 'serif')
+    plt.annotate(f"$norm_2 = {norm2}$", xy=(13, 0.18), xytext=(14, 0.175), fontsize = 19, fontname = 'serif')
+    plt.annotate(f"$norm_3 = {norm3}$", xy=(13, .145), xytext=(14, 0.16), fontsize = 19, fontname = 'serif')
+    plt.annotate(f"$norm_4 = {norm4}$", xy=(13, .135), xytext=(14, .145), fontsize = 19, fontname = 'serif')
+    plt.annotate(f"$norm_5 = {norm5}$", xy=(13, .13), xytext=(14, .13), fontsize = 19, fontname = 'serif')
+    plt.annotate(f"$norm_6 = {norm6}$", xy=(13, .145), xytext=(14, .115), fontsize = 19, fontname = 'serif')
+    plt.annotate(sigma, xy=(13, .145), xytext=(14, .1), fontsize = 19, fontname = 'serif')
+    plt.annotate(cstat, xy=(13, .14), xytext=(14, .085), fontsize = 19, fontname = 'serif')
+    plt.annotate(bins, xy=(13, .13), xytext=(16.3, .085), fontsize = 19, fontname = 'serif')
+    plt.annotate(flux, xy=(13, .055), xytext=(14, .07), fontsize = 19, fontname = 'serif')
+    plt.xlabel('Wavelength (Å)', fontsize = 19, fontname = 'serif')
+    plt.ylabel('Counts (s$^{-1}$ Å$^{-1}$)', fontsize = 19, fontname = 'serif')
+    plt.savefig(save_path_meg)
     plt.show()
 
 def heg_action():
@@ -95,24 +95,24 @@ def heg_action():
     plt.ylim(0, .12)
     plt.plot(histwvs[1], histmods[1], color='orange', linewidth=2, label = "model")
     plt.errorbar(hegwvs, hegvals, xerr=hegbins, yerr=hegerrors, fmt='.', alpha=1, color='gray', linewidth=2)
-    plt.annotate('ζ pup HEG group10_HEII 1', xy=(13, .115), xytext=(4.8, .115), fontsize = 18, fontname = 'serif')
-    plt.annotate(ObsID, xy=(13, .076), xytext=(4.8, .1), fontsize = 18, fontname = 'serif')
+    plt.annotate('ζ pup HEG group10_HEII 1', xy=(13, .115), xytext=(4.8, .115), fontsize = 19, fontname = 'serif')
+    plt.annotate(ObsID, xy=(13, .076), xytext=(4.8, .1), fontsize = 19, fontname = 'serif')
     plt.annotate(C, xy=(13, .06), xytext=(7.5, .115), fontsize = 17, fontname = 'serif')
     plt.annotate(N, xy=(13, .06), xytext=(8.5, .115), fontsize = 17, fontname = 'serif')
     plt.annotate(O, xy=(13, .06), xytext=(9.5, .115), fontsize = 17, fontname = 'serif')
-    plt.annotate(norms[0], xy=(10, .075), xytext=(11, .115), fontsize = 18, fontname = 'serif')
-    plt.annotate(norms[1], xy=(13, 0.08), xytext=(11, 0.105), fontsize = 18, fontname = 'serif')
-    plt.annotate(norms[2], xy=(13, .075), xytext=(11, 0.095), fontsize = 18, fontname = 'serif')
-    plt.annotate(norms[3], xy=(13, .07), xytext=(11, .085), fontsize = 18, fontname = 'serif')
-    plt.annotate(norms[4], xy=(13, .065), xytext=(11, .075), fontsize = 18, fontname = 'serif')
-    plt.annotate(norms[5], xy=(13, .06), xytext=(11, .065), fontsize = 18, fontname = 'serif')
-    plt.annotate(sigma, xy=(13, .055), xytext=(11, .055), fontsize = 18, fontname = 'serif')
-    plt.annotate(cstat, xy=(13, .05), xytext=(11, .045), fontsize = 18, fontname = 'serif')
-    plt.annotate(bins, xy=(13, .05), xytext=(12.3, .045), fontsize = 18, fontname = 'serif')
-    plt.annotate(flux, xy=(13, .055), xytext=(11, 0.035), fontsize = 18, fontname = 'serif')
-    plt.xlabel('Wavelength (Å)', fontsize = 18, fontname = 'serif')
-    plt.ylabel('Counts s$^{-1}$ Å$^{-1}$', fontsize = 18, fontname = 'serif')
-    #plt.savefig(save_path_heg)
+    plt.annotate(f"$norm_1 = {norm1}$", xy=(10, .075), xytext=(11, .115), fontsize = 19, fontname = 'serif')
+    plt.annotate(f"$norm_2 = {norm2}$", xy=(13, 0.08), xytext=(11, 0.105), fontsize = 19, fontname = 'serif')
+    plt.annotate(f"$norm_3 = {norm3}$", xy=(13, .075), xytext=(11, 0.095), fontsize = 19, fontname = 'serif')
+    plt.annotate(f"$norm_4 = {norm4}$", xy=(13, .07), xytext=(11, .085), fontsize = 19, fontname = 'serif')
+    plt.annotate(f"$norm_5 = {norm5}$", xy=(13, .065), xytext=(11, .075), fontsize = 19, fontname = 'serif')
+    plt.annotate(f"$norm_6 = {norm6}$", xy=(13, .06), xytext=(11, .065), fontsize = 19, fontname = 'serif')
+    plt.annotate(sigma, xy=(13, .055), xytext=(11, .055), fontsize = 19, fontname = 'serif')
+    plt.annotate(cstat, xy=(13, .05), xytext=(11, .045), fontsize = 19, fontname = 'serif')
+    plt.annotate(bins, xy=(13, .05), xytext=(12.3, .045), fontsize = 19, fontname = 'serif')
+    plt.annotate(flux, xy=(13, .055), xytext=(11, 0.035), fontsize = 19, fontname = 'serif')
+    plt.xlabel('Wavelength (Å)', fontsize = 19, fontname = 'serif')
+    plt.ylabel('Counts (s$^{-1}$ Å$^{-1}$)', fontsize = 19, fontname = 'serif')
+    plt.savefig(save_path_heg)
     plt.show()
     
 def both_action():
@@ -126,24 +126,24 @@ def both_action():
     plt.plot(histwvs[1], histmods[1], color='orange', linewidth=4, label = "model")
     plt.errorbar(hegwvs, hegvals, xerr=hegbins, yerr=hegerrors, fmt='.', alpha=0.4, color='gray', linewidth=2)
     #annotations
-    plt.annotate('ζ pup group10_HEII 1', xy=(13, .15), xytext=(5.1, .19), fontsize = 18, fontname = 'serif')
-    plt.annotate(ObsID, xy=(13, .15), xytext=(5.1, .17), fontsize = 18, fontname = 'serif')
+    plt.annotate('ζ pup group10_HEII 1', xy=(13, .15), xytext=(5.1, .19), fontsize = 19, fontname = 'serif')
+    plt.annotate(ObsID, xy=(13, .15), xytext=(5.1, .17), fontsize = 19, fontname = 'serif')
     plt.annotate(C, xy=(13, .06), xytext=(9, .19), fontsize = 17, fontname = 'serif')
     plt.annotate(N, xy=(13, .06), xytext=(10.5, .19), fontsize = 17, fontname = 'serif')
     plt.annotate(O, xy=(13, .06), xytext=(12, .19), fontsize = 17, fontname = 'serif')
-    plt.annotate(norms[0], xy=(13, .15), xytext=(14, 0.19), fontsize = 18, fontname = 'serif')
-    plt.annotate(norms[1], xy=(13, 0.18), xytext=(14, 0.175), fontsize = 18, fontname = 'serif')
-    plt.annotate(norms[2], xy=(13, .145), xytext=(14, 0.16), fontsize = 18, fontname = 'serif')
-    plt.annotate(norms[3], xy=(13, .135), xytext=(14, .145), fontsize = 18, fontname = 'serif')
-    plt.annotate(norms[4], xy=(13, .13), xytext=(14, .13), fontsize = 18, fontname = 'serif')
-    plt.annotate(norms[5], xy=(13, .145), xytext=(14, .115), fontsize = 18, fontname = 'serif')
-    plt.annotate(sigma, xy=(13, .145), xytext=(14, .1), fontsize = 18, fontname = 'serif')
-    plt.annotate(cstat, xy=(13, .14), xytext=(14, .085), fontsize = 18, fontname = 'serif')
-    plt.annotate(bins, xy=(13, .13), xytext=(16.3, .085), fontsize = 18, fontname = 'serif')
-    plt.annotate(flux, xy=(13, .055), xytext=(14, .07), fontsize = 18, fontname = 'serif')
+    plt.annotate(f"$norm_1 = {norm1}$", xy=(13, .15), xytext=(14, 0.19), fontsize = 19, fontname = 'serif')
+    plt.annotate(f"$norm_2 = {norm2}$", xy=(13, 0.18), xytext=(14, 0.175), fontsize = 19, fontname = 'serif')
+    plt.annotate(f"$norm_3 = {norm3}$", xy=(13, .145), xytext=(14, 0.16), fontsize = 19, fontname = 'serif')
+    plt.annotate(f"$norm_4 = {norm4}$", xy=(13, .135), xytext=(14, .145), fontsize = 19, fontname = 'serif')
+    plt.annotate(f"$norm_5 = {norm5}$", xy=(13, .13), xytext=(14, .13), fontsize = 19, fontname = 'serif')
+    plt.annotate(f"$norm_6 = {norm6}$", xy=(13, .145), xytext=(14, .115), fontsize = 19, fontname = 'serif')
+    plt.annotate(sigma, xy=(13, .145), xytext=(14, .1), fontsize = 19, fontname = 'serif')
+    plt.annotate(cstat, xy=(13, .14), xytext=(14, .085), fontsize = 19, fontname = 'serif')
+    plt.annotate(bins, xy=(13, .13), xytext=(16.3, .085), fontsize = 19, fontname = 'serif')
+    plt.annotate(flux, xy=(13, .055), xytext=(14, .07), fontsize = 19, fontname = 'serif')
     plt.xlabel('Wavelength (Å)', fontsize = 19, fontname = 'serif')
-    plt.ylabel('Counts s$^{-1}$ Å$^{-1}$', fontsize = 19, fontname = 'serif')
-    #plt.savefig(save_path_both)
+    plt.ylabel('Counts (s$^{-1}$ Å$^{-1}$)', fontsize = 19, fontname = 'serif')
+    plt.savefig(save_path_both)
     plt.show()
 
 meg_action()    
